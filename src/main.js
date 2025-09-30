4b196256bf02bf9725edb4b8a320ce40c1174b3d
Pedro Nora <pedro@nora.vc>
2025-09-30 10:47:50 -0300

    feat: inseridas configurações para bootstrap/bootstrap-icons

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia'
+import router from './router'
 import App from './App.vue'
+import * as bootstrap from 'bootstrap/dist/js/bootstrap.bundle';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './assets/main.css';
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.provide('bootstrap', bootstrap);
+
+app.mount('#app')
