dba99adec8c1dafc9de65521c55eedeac2e61ec6
Pedro Nora <pedro@nora.vc>
2025-10-02 18:21:15 -0300

    feat(router): adicionar rotas de users (list/create/detail/edit) e verificação de permissão

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,42 @@ import { createRouter, createWebHistory } from "vue-router";
 import { useAuthStore } from "@/stores/auth";
 
 const routes = [
-    { path: "/login", name: "Login", component: () => import('@/pages/Login.vue') },
-    { path: "/", name: "Home", component: () => import('@/pages/users/List.vue'), meta: { requiresAuth: true } },
+    {
+        path: "/login",
+        name: "Login",
+        component: () => import('@/pages/Login.vue')
+    },
+    {
+        path: "/",
+        name: "Home",
+        component: () => import('@/pages/Home.vue')
+    },
+    {
+        path: '/users',
+        name: 'UsersList',
+        component: () => import('@/pages/users/List.vue'),
+        meta: { requiresAuth: true, requiresPermission: true }
+    },
+    {
+        path: '/users/create',
+        name: 'UsersCreate',
+        component: () => import('@/pages/users/Create.vue'),
+        meta: { requiresAuth: true, requiresPermission: true }
+    },
+    {
+        path: '/users/:id',
+        name: 'UsersDetail',
+        component: () => import('@/pages/users/Detail.vue'),
+        props: true,
+        meta: { requiresAuth: true, requiresPermission: true }
+    },
+    {
+        path: '/users/:id/edit',
+        name: 'UsersEdit',
+        component: () => import('@/pages/users/Update.vue'),
+        props: true,
+        meta: { requiresAuth: true, requiresPermission: true }
+    }
 ];
 
 const router = createRouter({
@@ -17,6 +51,10 @@ router.beforeEach((to, from, next) => {
 
     if (to.meta.requiresAuth && !auth.isAuthenticated) {
         return next({ name: "Login", query: { next: to.fullPath } });
+    }
+
+    if (to.meta.requiresPermission && !['coord', 'admin'].includes(auth.user.role)) {
+        return next({ name: "Home" })
     }
     next();
 });
